9800d1a7a1fdaf1980d0c21c145e09b3ad6e2520
ChNira2024 <niranjanacharty1@gmail.com>
2026-05-15 20:58:45 +0530

    k8 config

--- price-service/src/main/java/com/niranjana/price/config/CorsConfig.java
+++ price-service/src/main/java/com/niranjana/price/config/CorsConfig.java
@@ -1,5 +1,8 @@
 package com.niranjana.price.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,20 +12,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${frontend.url}")
-    private String frontendUrl;
+	@Value("${frontend.url}")
+	private String frontendUrl;
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                System.out.println("CORS frontendUrl: " + frontendUrl);
-                registry.addMapping("/**")
-                        .allowedOrigins(frontendUrl)
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+	@Value("${frontend.url.k8s:#{null}}")
+	private String frontendUrlK8s;
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            List<String> origins = new ArrayList<>();
+	            origins.add(frontendUrl);
+	            if (frontendUrlK8s != null) {
+	                origins.add(frontendUrlK8s);
+	            }
+	            registry.addMapping("/**")
+	                    .allowedOrigins(origins.toArray(new String[0]))
+	                    .allowedMethods("*")
+	                    .allowedHeaders("*");
+	        }
+	    };
+	}
 }
